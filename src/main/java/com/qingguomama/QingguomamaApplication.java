8b56af3a4e1ff1a185aaf564ca01a8c781e74ec0
fuliang81 <1078229370@qq.com>
2019-01-11 17:38:48 +0800

    mysql+mybaits整合半成品

--- src/main/java/com/qingguomama/QingguomamaApplication.java
+++ src/main/java/com/qingguomama/QingguomamaApplication.java
@@ -3,8 +3,11 @@ package com.qingguomama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@MapperScan("com.qingguomama.dao")
+//@ComponentScan(basePackages = {"com.qingguomama.*"})
 public class QingguomamaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/qingguomama/bean/Activity.java
@@ -0,0 +1,9 @@
+package com.qingguomama.bean;
+
+public class Activity {
+    private String objectId;
+    private String date;
+    private String content;
+    private String release;
+
+}
